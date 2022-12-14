4b3121771ca5431d3ec374c149ba8c2b1bc75aec
thomasluizon <thomaslrgregorio@gmail.com>
2022-12-13 23:03:09 -0300

    feat: added test when user enters wrong cnpj

--- app/adapters/cnpj.js
+++ app/adapters/cnpj.js
@@ -2,7 +2,7 @@ import RESTAdapter from '@ember-data/adapter/rest';
 
 export default class CnpjAdapter extends RESTAdapter {
   urlForFindRecord(cnpj) {
-    const apiMode = false;
+    const apiMode = true;
     return apiMode
       ? `https://api.nfse.io/LegalEntities/Basicinfo/taxNumber/${cnpj}`
       : '/api/api.json';
